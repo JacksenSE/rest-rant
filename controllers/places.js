3522f600f0eec4f564f246f095d9549eccf59924
JacksenSE <Jacskenbmarshall@gmail.com>
2022-12-03 10:55:10 -0800

    Made Changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,40 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-  }]
-  
-  res.render('places/index', { places })
-
+    res.render('places/index', { places })
 })
+
+router.post('/', (req, res) => {
+    if (!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  }) 
+  
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+    res.render('places/show', { place: places[id],id})
+    }
+  })
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/dog-1.jpg'
+}, {
+    name: 'Cafe',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'Coffee, Food',
+    pic: '/images/dog-2.jpg'
+}]
